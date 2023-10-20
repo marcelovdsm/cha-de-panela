ebd226b8c6e30180505a8704a541d742e7fcbdfc
Marcelo Victor <95694179+marcelovdsm@users.noreply.github.com>
2023-10-20 13:10:04 +0000

    structure added

--- src/components/InspirationsPage/index.tsx
+++ src/components/InspirationsPage/index.tsx
@@ -1,7 +1,74 @@
 export const Inspirations = () => {
   return (
     <>
-      <h1></h1>
+      <h1>Inspire-se</h1>
+      <p>
+        Aqui deixamos alguns exemplos do que gostamos de ganhar, mas não
+        necessáriamente precisa ser igual. Fique a vontade!
+      </p>
+      <ul>
+        <li>
+          <img
+            src="https://picsum.photos/200"
+            alt="Imagem de alguns exemplos do que gostariamos de ganhar."
+          />
+        </li>
+        <li>
+          <img
+            src="https://picsum.photos/200"
+            alt="Imagem de alguns exemplos do que gostariamos de ganhar."
+          />
+        </li>
+        <li>
+          <img
+            src="https://picsum.photos/200"
+            alt="Imagem de alguns exemplos do que gostariamos de ganhar."
+          />
+        </li>
+        <li>
+          <img
+            src="https://picsum.photos/200"
+            alt="Imagem de alguns exemplos do que gostariamos de ganhar."
+          />
+        </li>
+        <li>
+          <img
+            src="https://picsum.photos/200"
+            alt="Imagem de alguns exemplos do que gostariamos de ganhar."
+          />
+        </li>
+        <li>
+          <img
+            src="https://picsum.photos/200"
+            alt="Imagem de alguns exemplos do que gostariamos de ganhar."
+          />
+        </li>
+        <li>
+          <img
+            src="https://picsum.photos/200"
+            alt="Imagem de alguns exemplos do que gostariamos de ganhar."
+          />
+        </li>
+        <li>
+          <img
+            src="https://picsum.photos/200"
+            alt="Imagem de alguns exemplos do que gostariamos de ganhar."
+          />
+        </li>
+        <li>
+          <img
+            src="https://picsum.photos/200"
+            alt="Imagem de alguns exemplos do que gostariamos de ganhar."
+          />
+        </li>
+      </ul>
+      <div>
+        <h4>Escolhemos as seguintes cores para a nossa casa:</h4>
+        <p>Cor1</p>
+        <p>Cor2</p>
+        <p>Cor3</p>
+        <p>Cor4</p>
+      </div>
     </>
   )
 }
