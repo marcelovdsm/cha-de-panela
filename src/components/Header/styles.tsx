d063329043c950d1679cd1b66a4c7b8c226ad820
Marcelo Victor <marcelovictorsm077@gmail.com>
2023-10-29 19:32:38 -0300

    last changes

--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components'
 
 export const HeaderStyles = styled.div`
+  position: sticky;
+  top: 0;
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 0.7rem;
   color: ${(props) => props.theme.darkGreen};
   background-color: ${(props) => props.theme.lightGreen};
+  border-bottom: 2px solid ${(porps) => porps.theme.darkGreen};
 `
